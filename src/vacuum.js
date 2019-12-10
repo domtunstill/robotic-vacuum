899c5d71989981ee3446a550e7bb81de24f3830c
Dom Tunstill <datunstill@gmail.com>
2019-12-10 23:37:32 +0000

    tests to mkae sure vacuum cannot go outside room dimensions

--- src/vacuum.js
+++ src/vacuum.js
@@ -5,10 +5,7 @@ const moves = { N: 1, S: -1, E: 1, W: -1};
 function moveVacuum(position, move, roomDims){
   if (move === 'E' || move === 'W'){
     var newXCoord = position[0] + moves[move];
-    // console.log(newXCoord);
-    // console.log(roomDims[1]);
     if (newXCoord >= 0 && newXCoord < roomDims[0]){
-      // console.log(newXCoord);
       position[0] = newXCoord;
     }
   };
