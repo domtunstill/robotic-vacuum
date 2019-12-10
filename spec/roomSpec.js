d540c1611795ae3e22a9e6abb213dcf77281c89d
Dom Tunstill <datunstill@gmail.com>
2019-12-10 13:22:52 +0000

    pass third test for converting the input into an array

--- spec/roomSpec.js
+++ spec/roomSpec.js
@@ -16,6 +16,11 @@ describe("Room", function(){
         expect(room.formattedInput).toEqual(['5 5 ','1 2 '])
     });
 
+    it ('converts input into array based on lines', function(){
+        room = new roomFunction('5 5 \n1 2 \nNNESEESWNWW');
+        expect(room.formattedInput).toEqual(['5 5 ','1 2 ','NNESEESWNWW'])
+    });
+
     // it ('takes the room dimensions from the first line of the input', function(){
     //     room = new roomFunction('5 5 ');
     //     expect(room.x_dimension).toBe(5)
