c80f2d5e12ef8a5ddf3dd873a8252038754abaac
Dom Tunstill <datunstill@gmail.com>
2019-12-11 01:39:03 +0000

    running node up nows saves the output to output.txt file

--- src/cleaningReport.js
+++ src/cleaningReport.js
@@ -1,10 +1,19 @@
 'use strict';
+var fs = require('fs');
+
 function createCleaningReport(finalPosition, numberCleaned){
   return finalPosition + '\n' + numberCleaned;
 };
 
 function printCleaningReport(report){
   console.log(report);
+  
+  fs.writeFile('output.txt', report, function(err) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log('The file was saved!');
+  });
 };
 
 module.exports.createCleaningReport = createCleaningReport;
